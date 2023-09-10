cc1be2f55de78d12fdd30d9ae0ff4cc216002307
Harshith-reddy1629 <reddybharshith3@gmail.com>
2023-09-10 12:38:54 +0000

    commit 3

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+
 const { open } = require("sqlite");
+
 const sqlite3 = require("sqlite3");
+
 const path = require("path");
+
 const bcrypt = require("bcrypt");
+
 const jwt = require("jsonwebtoken");
 
 const databasePath = path.join(__dirname, "covid19IndiaPortal.db");
@@ -11,17 +16,20 @@ const app = express();
 
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 let database = null;
 
 const initializeDbAndServer = async () => {
   try {
     database = await open({
       filename: databasePath,
+
       driver: sqlite3.Database,
     });
 
-    app.listen(3000, () =>
-      console.log("Server Running at http://localhost:3000/")
+    app.listen(PORT, () =>
+      console.log(`Server Running at http://localhost:${PORT}/`)
     );
   } catch (error) {
     console.log(`DB Error: ${error.message}`);
