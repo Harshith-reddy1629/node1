2fb0d920542b1060dcc6bb03cc0bcdfe6c9ed821
Harshith-reddy1629 <reddybharshith3@gmail.com>
2023-09-10 16:02:51 +0000

    commit 6

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ function authenticateToken(request, response, next) {
   }
 }
 
-app.post("/login/", async (request, response) => {
+app.post("/login/", cors(), async (request, response) => {
   const { username, password } = request.body;
   const selectUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
   const databaseUser = await database.get(selectUserQuery);
